90ebcc9e622e663ad610e2525c9e9599c6a6ee22
belendi1999 <97689725+belendi1999@users.noreply.github.com>
2022-06-27 20:34:03 +0200

    falta la ultima iteracion, no sé hacerla

--- routes/drones.js
+++ routes/drones.js
@@ -25,18 +25,50 @@ router.post('/drones/create', (req, res, next) => {
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+const {id} = req.params
+console.log(id)
+Drone.findById(id)
+.then((response) => {
+  res.render('drones/update-form.hbs', response)
+})
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+  const {id} = req.params
+  console.log(req.body.name)
+
+  Drone.findByIdAndUpdate(id, {"name": req.body.name}, function(err, result){
+
+    if(err){
+        res.send(err)
+    }
+    else{
+      Drone.find()
+      .then (response => {
+        console.log(response)
+        res.render("drones/list", { drone: response})
+        
+      })
+      .catch((err) => {
+        next(err)
+      });
+    }
+
+})
+
+  
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
-});
+router.post('drone-delete/:id', (req, res, next) => {
+
+    Drone
+      .deleteOne({id:req.params.id})
+      .then(response =>{
+        console.log("Eliminado correctamente")
+        res.redirect("/drones")
+      })
+      .catch(e=>console.log(e))
+  });
+
 
 module.exports = router;
